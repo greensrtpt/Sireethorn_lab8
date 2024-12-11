90c0beaf102eefa528da749f2cd54e0674db9d76
sireethorn <sireethorn2548.h20p@gmail.com>
2024-12-11 11:06:14 +0700

    lab8_3(Compile).cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age > 60) {
+        cout << "Your character = Brook" << endl;
+    } else if (age > 25) {
+        long long bounty;
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+
+        if (bounty > 500000000) {
+            cout << "Your character = Jinbe" << endl;
+        } else {
+            cout << "Your character = Franky" << endl;
+        }
+    } else {
+        int height;
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if (height < 100) {
+            cout << "Your character = Chopper" << endl;
+        } else if (height >= 100 && height < 180) {
+            cout << "Your character = Usopp" << endl;
+        } else {
+            long long bounty;
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+
+            if (bounty > 1100000000) {
+                cout << "Your character = Zoro" << endl;
+            } else {
+                cout << "Your character = Sanji" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
